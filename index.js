c4df371394778beeb146de46dfa3e7813aa63cec
freak906 <119906147+nick-vns@users.noreply.github.com>
2023-02-06 15:56:22 -0700

    added origin

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 const cors = require('cors');
 
-let allowedOrigins = ['*'];
+let allowedOrigins = ['http://localhost:1234'];
 app.use(cors({
     origin: (origin, callback) => {
         if (!origin) return callback(null, true);
