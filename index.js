ff01373eb2d3dbfb49f0118e4d2ba5ff377ccc54
freak906 <119906147+nick-vns@users.noreply.github.com>
2023-02-23 11:04:00 -0700

    disabled cors

--- index.js
+++ index.js
@@ -24,27 +24,27 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const cors = require("cors");
-//app.use(cors());
+app.use(cors());
 
-let allowedOrigins = [
-  "http://localhost:1234",
-  "http://localhost:8080",
-  "https://cinemaflix-movies.netlify.app/",
-];
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        let message =
-          "The CORS policy for this application doesn't allow access from origin " +
-          origin;
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
+// let allowedOrigins = [
+//   "http://localhost:1234",
+//   "http://localhost:8080",
+//   "https://cinemaflix-movies.netlify.app/",
+// ];
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         let message =
+//           "The CORS policy for this application doesn't allow access from origin " +
+//           origin;
+//         return callback(new Error(message), false);
+//       }
+//       return callback(null, true);
+//     },
+//   })
+// );
 
 let auth = require("./auth")(app);
 const passport = require("passport");
